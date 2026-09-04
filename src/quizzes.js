e5146bc7431083a9dfd825fef12351c64bf158ae
thamidu-nadun <nadunrz101@gmail.com>
2026-09-04 10:20:38 +0530

    feat: add NS quizzes

--- src/quizzes.js
+++ src/quizzes.js
@@ -1,4 +1,41 @@
 export default [
+  // Network Security Quizzes
+  {
+    title: "Network Security - 06",
+    path: "quiz/NS/NS-006.json",
+    description: "",
+    isAvailable: true,
+  },
+  {
+    title: "Network Security - 05",
+    path: "quiz/NS/NS-005.json",
+    description: "",
+    isAvailable: true,
+  },
+  {
+    title: "Network Security - 04",
+    path: "quiz/NS/NS-004.json",
+    description: "",
+    isAvailable: true,
+  },
+  {
+    title: "Network Security - 03",
+    path: "quiz/NS/NS-003.json",
+    description: "",
+    isAvailable: true,
+  },
+  {
+    title: "Network Security - 02",
+    path: "quiz/NS/NS-002.json",
+    description: "",
+    isAvailable: true,
+  },
+  {
+    title: "Network Security - 01",
+    path: "quiz/NS/NS-001.json",
+    description: "",
+    isAvailable: true,
+  },
   // Modern Enterprise Management Quizzes
   {
     title: "Modern Enterprise Management - 07",
